4b0a0347cd45509ccb2ecd32a0d917c518f3e450
Francisco Soto <Francisco.stht@gmail.com>
2020-08-18 17:58:26 -0400

    Se agrega el manejo de errores de errores para codigo asincronos no manejados

--- server.js
+++ server.js
@@ -1,28 +1,36 @@
-const mongose = require('mongoose')
-const dotenv = require('dotenv')
-const app = require('./app')
-
-
+const mongose = require("mongoose");
+const dotenv = require("dotenv");
+const app = require("./app");
 
 dotenv.config({
-    path: './.env'
-})
+  path: "./.env",
+});
 console.log(process.env.NODE_ENV);
 const DB = process.env.DATABASE.replace(
-    '<PASSWORD>',
-    process.env.DATABASE_PASSWORD
-)
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
 
 mongose
-    .connect(DB, {
-        useNewUrlParser: true,
-        useCreateIndex: true
-    })
-    .then(() => console.log("DB connection successful 🚀 ⚡️ "))
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("DB connection successful 🚀 ⚡️ "));
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 //Iniciamos el servidor y estamos escuchando con Listen
-app.listen(port, () => {
-    console.log(`App running on port ${port}...`)
-})
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+//agregamos este process para escuchar y para manejar la proimesa(codigo asincrono) no manejada
+//como una ultima red de seguridad
+//en este caso estamos probando un error con la BD y si tenemos un error cerramos la aplicacion
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLER REJECTION shutting down ");
+  server.close(() => {
+    process.exit(1);
+  });
+});
