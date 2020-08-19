5c507ab3ee77197dfe98c275978e81d9f3e6cdd4
Francisco SotoZ <francisco.stht@gmail.com>
2020-08-18 23:50:12 -0400

    se agregan validaciones globales para capturar errores asincronos y sincronos

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const mongose = require('mongoose')
 const dotenv = require('dotenv')
 const app = require('./app')
 
+//excepciones no manejadas codigo sincrono, por ejemplo:
 
+process.on('uncaughtException', err => {
+    console.log('Ucaught excepction ! 🔥 shutting down...')
+    console.log(err.name, err.message)
+
+    process.exit(1)
+
+})
 
 dotenv.config({
     path: './.env'
@@ -21,10 +29,26 @@ mongose
 
     })
     .then(() => console.log("DB connection successful 🚀 ⚡️ "))
-
+// .catch(err => console.log('Huno un error'))
 const port = process.env.PORT || 3000
 
 //Iniciamos el servidor y estamos escuchando con Listen
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App running on port ${port}...`)
 })
+
+//una promesa no manejada significa que en algun lugar de nuestro codigo una promesa fue rechazada
+//podemos solucionar esto con un catch pero puedo agregar una funcion que maneje el global de este tipo de error
+//con este on estamos escuchando los evento
+//es como nuestra ultima red de serguridad
+process.on('unhandledRejection', err => {
+    //para cerra la aplicacion podemos ocupar process .ext
+    console.log('Unhandler rejection ! 🔥 shutting down...')
+    console.log(err.name, err.message)
+    server.close(() => {
+
+        process.exit(1)
+    })
+})
+
+// console.log(x)
