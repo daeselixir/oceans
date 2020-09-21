07e134a7a0bf481732bd43e5f40b2ebdfe9178b3
Francisco Soto <Francisco.stht@gmail.com>
2020-09-21 17:27:58 -0300

    se agregan los controller history con sus 2 consultas por id y listar todas las historias de los ticket

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -1,13 +1,15 @@
 const Ticket = require("../models/ticket.js");
 const catchAsync = require("../validator/catchAsync");
 const mongoose = require("mongoose");
+var ObjectId = require("mongodb").ObjectID;
 
 //Conexion coleccion Historial
 var Schema = mongoose.Schema;
 mongoose.connect(
-  "mongodb+srv://daeselixir:mahuida1106@cluster0.to4nx.mongodb.net/oceans?retryWrites=true&w=majority", {
+  "mongodb+srv://daeselixir:mahuida1106@cluster0.to4nx.mongodb.net/oceans?retryWrites=true&w=majority",
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   },
   function (err) {
     if (err) throw err;
@@ -17,9 +19,12 @@ mongoose.connect(
 );
 
 var db = mongoose.connection;
-var MyModel = db.model("histories", new Schema({
-  name: String
-}));
+var MyModel = db.model(
+  "histories",
+  new Schema({
+    name: String,
+  })
+);
 
 // Consultas
 
@@ -34,6 +39,7 @@ exports.ticketId = catchAsync(async (req, res, next) => {
   req.params = ticket;
   next();
 });
+
 exports.readId = (req, res) => {
   return res.json(req.params);
 };
@@ -49,21 +55,48 @@ exports.listTicket = (req, res) => {
   });
 };
 
-exports.getHistorialId = catchAsync(async (req, res, next) => {
-  MyModel.find({}, function (err, historial) {
-    Ticket.populate(historial, {
-        collectionId: '_id',
-
-      },
-      function (err, historial) {
-        res.status(200).json(historial);
-
+exports.historialList = async (req, res) => {
+  await MyModel.find({}, function (err, data) {
+    if (!data) {
+      return res.status(401).json({
+        error: "histories not found",
       });
-
+    }
+    return res.json({
+      status: "success",
+      results: data.length,
+      histories: data,
+    });
   });
-})
+};
 
+exports.historialId = catchAsync(async (req, res, next) => {
+  await MyModel.find(
+    { collectionId: ObjectId(req.params.historyId) },
+    function (err, data) {
+      if (!data) {
+        return res.status(401).json({
+          err: "histories not found",
+        });
+      }
+      req.params = data;
+      next();
+      /*  return res.json({
+        status: "success",
+        result: data.length,
+        data,
+      });*/
+    }
+  );
+});
 
+exports.readHistoryId = (req, res) => {
+  let history = req.params;
+  return res.json({
+    results: history.length,
+    history,
+  });
+};
 
 exports.createTicket = catchAsync(async (req, res) => {
   const newTicket = await Ticket.create(req.body);
--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -1,24 +1,27 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 const {
-    readId,
-    ticketId,
-    listTicket,
-    createTicket,
-    updateTicket,
-    deleteTicket,
-    getHistorialId
-} = require('../controllers/ticketController')
+  readId,
+  ticketId,
+  listTicket,
+  createTicket,
+  updateTicket,
+  deleteTicket,
+  historialList,
+  historialId,
+  readHistoryId,
+} = require("../controllers/ticketController");
 
-router.get('/ticket', listTicket)
-router.get('/ticket/historial', getHistorialId)
-router.get('/ticket/:ticketId', readId)
-router.post('/ticket/create', createTicket)
-router.put('/ticket/:ticketId', updateTicket)
-router.delete('/ticket/:ticketId', deleteTicket)
+router.get("/ticket", listTicket);
+router.get("/ticket/historial", historialList);
+router.get("/ticket/:ticketId", readId);
+router.get("/ticket/historial/:historyId", readHistoryId);
+router.post("/ticket/create", createTicket);
+router.put("/ticket/:ticketId", updateTicket);
+router.delete("/ticket/:ticketId", deleteTicket);
 
-router.param('ticketId', ticketId)
+router.param("ticketId", ticketId);
+router.param("historyId", historialId);
 
-
-module.exports = router
+module.exports = router;
